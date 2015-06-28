b3df2240add73499cff0b343cbcc611a46817f5f
Max Plomer <maxplomer@gmail.com>
2015-06-27 20:12:25 -0400

    add city class and player initialize method

--- player.rb
+++ player.rb
@@ -1,9 +1,17 @@
 class Player
 
-  attr_accessor :name, :money, :location, :apartment, :sex
-  attr_reader :jobs, :cats
+  attr_accessor :money, :location, :apartment
+  attr_reader :name, :sex, :jobs, :cats
 
-  def initialize
+  def initialize(name, sex)
+    @money = 10000
+    @location = 'suburbs'
+    @apartment = nil
+    @name = name
+    @sex = sex #leaving sex a user inputed string for now
+    @jobs = []
+    @cats = []
+  end
 
 
   # make sure job schedules dont conflict
