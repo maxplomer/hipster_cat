75a2ec05c6689003c4cea81bd6cc715ffa300511
Max Plomer <maxplomer@gmail.com>
2015-06-29 00:47:37 -0400

    the cities have apartments

--- apartment.rb
+++ apartment.rb
@@ -1,4 +1,36 @@
 class Apartment
 
+  ADJECTIVES = [
+    "tiny",
+    "smelly",
+    "spacious",
+    "industrial",
+    "chill",
+    "",
+    "groundlevel"
+  ]
+
+  TYPES = [
+    "studio",
+    "3 bedroom",
+    "apartment"
+  ]
+
+  AMMENITIES = [
+    "with a view",
+    "with a view of the water",
+    "might have a couple sewer flies no big deal",
+    "on the 40th floor",
+    "in an up and coming area",
+    "with a roommate who's in a band",
+    "close to the vegan restaurant",
+    "way on the other side of town"
+  ]
+
+  attr_reader :description
+
+  def initialize
+    @description = "A #{ADJECTIVES.sample} #{TYPES.sample} #{AMMENITIES.sample}"
+  end
 
 end
--- city.rb
+++ city.rb
@@ -1,5 +1,6 @@
 require_relative './cat'
 require_relative './job'
+require_relative './apartment'
 
 class City
   # cities should hold the jobs and cats and apartments
@@ -8,12 +9,13 @@ class City
     [City.new('the suburbs'), City.new('brooklyn'), City.new('LA')]
   end
 
-  attr_reader :name, :cats, :jobs
+  attr_reader :name, :cats, :jobs, :apartments
 
   def initialize(name)
     @name = name
     @cats = get_cats
     @jobs = get_jobs(name)
+    @apartments = get_apartments(name)
   end
 
   private
@@ -33,6 +35,16 @@ class City
 
     rand(10).times do 
       result << Job.new(city)
+    end
+
+    result
+  end
+
+  def get_apartments(city)
+    result = []
+
+    rand(10).times do 
+      result << Apartment.new
     end
 
     result
